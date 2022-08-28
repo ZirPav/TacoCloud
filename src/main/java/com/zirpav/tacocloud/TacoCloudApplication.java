43676b5e3bb54e159aaf072da94525e30b360d31
user <zir-pav@mail.ru>
2022-08-28 22:24:13 +0300

    Spring in Action 6 - start

---
+++ src/main/java/com/zirpav/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.zirpav.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
